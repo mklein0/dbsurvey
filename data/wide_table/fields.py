ba55d28987a2219ea21baadc0357c2774388f7ca
Marcos Klein <mklein0@gmail.com>
2021-06-03 18:21:12 -0700

    Add wide table sample data.

--- data/wide_table/fields.py
+++ data/wide_table/fields.py
@@ -7,22 +7,28 @@ from string import ascii_letters
 len_letters = len(ascii_letters)
 
 
-def wide_data():
+def wide_data(num_sets):
     return list(itertools.chain.from_iterable(
         # INT, STR, FLOAT
         [
             random.randrange(0, 1000),
             ''.join(ascii_letters[random.randrange(0, len_letters)] for _ in range(16)),
-            random.randrange(0, 500, step=0.5, _int=float),
+            random.randrange(0, 1000) * 0.5,
         ]
-        for i in range(3000)
+        for i in range(num_sets)
     ))
 
 
-with open('WIDE_TABLE.csv', 'w') as outfile:
+number = 100000
+label = '100K'
+
+with open('WIDE_TABLE_{}.csv'.format(label), 'w') as outfile:
     csvwriter = csv.writer(outfile)
-    with open('../web_stat/WEB_STAT.csv', 'rb') as infile:
+    with open('../web_stat/WEB_STAT.csv', 'r') as infile:
         csvreader = csv.reader(infile)
         for inrow in csvreader:
-            row = inrow[:-1] + wide_data() + inrow[-1:]
+            num_columns = number - len(inrow)
+            num_sets = (num_columns + 2) // 3
+            print((number, num_columns, num_sets, 3*num_sets+len(inrow)))
+            row = inrow[:-1] + wide_data(num_sets) + inrow[-1:]
             csvwriter.writerow(row)
